0ec8eabab9b600bcd8b98f16ae3db707e7409b2d
Davicprd <davicpradim@gmail.com>
2025-05-22 11:53:51 -0300

    Arrumando o Index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
 
-const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY});
 
 async function main() {
   const response = await ai.models.generateContent({
